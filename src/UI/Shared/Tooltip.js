0f14b4a811c94e2a570eca308f01ad425c4377de
Taloth Saldono <Taloth@users.noreply.github.com>
2015-02-12 22:23:41 +0100

    Tooltips should now be attached to a container close to the target element while avoiding button/input groups.

--- src/UI/Shared/Tooltip.js
+++ src/UI/Shared/Tooltip.js
@@ -1,8 +1,28 @@
 var $ = require('jquery');
 
+var Tooltip = $.fn.tooltip.Constructor;
+
+var origGetOptions = Tooltip.prototype.getOptions;
+Tooltip.prototype.getOptions = function(options) {
+    var result = origGetOptions.call(this, options);
+
+    if (result.container === false) {
+
+        var container = this.$element.closest('.btn-group,.input-group').parent();
+
+        if (container.length) {
+            result.container = container;
+        }
+    }
+
+    return result;
+};
+
 module.exports = {
-    appInitializer : function(){
+    appInitializer : function() {
+
         $('body').tooltip({selector : '[title]'});
+
         return this;
     }
 };
